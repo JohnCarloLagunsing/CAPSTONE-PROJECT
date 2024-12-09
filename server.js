9b2b1e2eae386bbfd8e575e6f8c99f6b019c3bee
JohnCarloLagunsing <jc6690304@gmail.com>
2024-12-09 21:10:54 +0800

    changing the seccure into false

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        secure: process.env.NODE_ENV === 'production', // Secure cookies in production
+        secure: false, // Secure cookies in production
         httpOnly: true,
         sameSite: 'none', // Allow cross-origin requests
     }    
